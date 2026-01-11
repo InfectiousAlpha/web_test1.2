12257f236c7dcbecb6eb014431334c83965dcd39
InfectiousAlpha <nurmaghribii@gmail.com>
2026-01-11 22:26:21 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+         indigo: { 600: '#4f46e5', 700: '#4338ca' },
+      }
+    },
+  },
+  plugins: [],
+}
